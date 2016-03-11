c45f255942bd572c684b891337635c028a85ab5d
kokokiko <zhroguexe@gmail.com>
2016-03-11 12:07:18 +0100

    Added files via upload

---
+++ Prove/javascript.js
@@ -0,0 +1,10 @@
+function eseguito() {
+    if  (document.getElementById("menu").style.visibility== "visible") {
+         document.getElementById("menu").style.visibility="hidden";
+    }else{  document.getElementById("menu").style.visibility= "visible";}
+}
+
+function e() {
+    alert("a");
+	 document.getElementsByid('tlt').textillate();
+}
