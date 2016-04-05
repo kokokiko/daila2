02207ad1c46f297c6ddc47da63060756a4e33476
kokokiko <zhroguexe@gmail.com>
2016-04-06 00:23:10 +0200

    Update javascript.js

--- Prove/javascript.js
+++ Prove/javascript.js
@@ -1,5 +1,1 @@
-function eseguito() {
-    if  (document.getElementById("menu").style.visibility== "visible") {
-         document.getElementById("menu").style.visibility="hidden";
-    }else{  document.getElementById("menu").style.visibility= "visible";}
-}
+
